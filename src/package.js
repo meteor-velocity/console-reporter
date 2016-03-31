eeaa2bd9f1625cf171512f6f881fa361e8377f45
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2016-03-31 16:19:10 -0700

    Release of 0.2.0

--- src/package.js
+++ src/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'velocity:console-reporter',
-  version: '0.2.0-rc.3',
+  version: '0.2.0',
   summary: 'A console reporter for Velocity',
   git: 'https://github.com/meteor-velocity/console-reporter.git',
   documentation: '../README.md',
@@ -8,11 +8,11 @@ Package.describe({
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.3-rc.3');
+  api.versionsFrom('1.3');
   api.export('ConsoleReporter', 'server');
   api.use([
     'underscore',
-    'velocity:reports@1.0.0-rc.3'
+    'velocity:reports@1.0.0'
   ], 'server');
   api.addFiles([
     'ConsoleReporter.js',
